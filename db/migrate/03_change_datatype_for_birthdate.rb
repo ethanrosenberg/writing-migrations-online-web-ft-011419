82f4ee0e9a639d3effc4fe66a797df5b426f00a7
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-02-25 19:52:14 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,6 +1,6 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
   def change
-    add_column :students do |t|
+    change_column :students do |t|
       t.integer :grade
       t.string :birthdate
     end
